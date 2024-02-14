346f6cec93581b2af9dcd29c217edd7cb4729cdf
sushtsdvg <sushtsdvg@gmail.com>
2024-02-14 10:28:15 +0530

    Models using mongoose

--- backend/src/models/productModel.ts
+++ backend/src/models/productModel.ts
@@ -1,41 +1,74 @@
-import { getModelForClass, modelOptions, prop } from "@typegoose/typegoose";
+// import { getModelForClass, modelOptions, prop } from "@typegoose/typegoose";
 
-@modelOptions({ schemaOptions: { timestamps: true } })
-export class Product {
-  public _id?: string;
+// @modelOptions({ schemaOptions: { timestamps: true } })
+// export class Product {
+//   public _id?: string;
 
-  @prop({ required: true })
-  public name!: string;
+//   @prop({ required: true })
+//   public name!: string;
 
-  @prop({ required: true, unique: true })
-  public slug!: string;
+//   @prop({ required: true, unique: true })
+//   public slug!: string;
 
-  @prop({ required: true })
-  public image!: string;
+//   @prop({ required: true })
+//   public image!: string;
 
-  @prop({ required: true })
-  public brand!: string;
+//   @prop({ required: true })
+//   public brand!: string;
 
-  @prop({ required: true })
-  public category!: string;
+//   @prop({ required: true })
+//   public category!: string;
 
-  @prop({ required: true })
-  public description!: string;
+//   @prop({ required: true })
+//   public description!: string;
 
-  @prop({ required: true, default: 0 })
-  public price!: number;
+//   @prop({ required: true, default: 0 })
+//   public price!: number;
 
-  @prop({ required: true, default: 0 })
-  public countInStock!: number;
+//   @prop({ required: true, default: 0 })
+//   public countInStock!: number;
 
-  @prop({ required: true, default: 0 })
-  public rating!: number;
+//   @prop({ required: true, default: 0 })
+//   public rating!: number;
 
-  @prop({ required: true, default: 0 })
-  public numReviews!: number;
+//   @prop({ required: true, default: 0 })
+//   public numReviews!: number;
 
-  @prop({ required: true, default: 0 })
-  public discount!: number;
+//   @prop({ required: true, default: 0 })
+//   public discount!: number;
+// }
+
+// export const ProductModel = getModelForClass(Product);
+
+import { Schema, model } from "mongoose";
+
+export interface IProduct {
+  _id: string;
+  name: string;
+  slug: string;
+  image: string;
+  brand: string;
+  category: string;
+  description: string;
+  price: number;
+  countInStock: number;
+  rating: number;
+  numReviews: number;
+  discount: number;
 }
 
-export const ProductModel = getModelForClass(Product);
+export const productSchema = new Schema<IProduct>({
+  name: { type: String, required: true },
+  slug: { type: String, required: true, unique: true },
+  image: { type: String, required: true },
+  brand: { type: String, required: true },
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  countInStock: { type: Number, required: true },
+  rating: { type: Number, required: true },
+  numReviews: { type: Number, required: true },
+  discount: { type: Number, required: true },
+});
+
+export const ProductModel = model<IProduct>("Product", productSchema);
--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,20 +1,39 @@
-import { getModelForClass, modelOptions, prop } from "@typegoose/typegoose";
+// import { getModelForClass, modelOptions, prop } from "@typegoose/typegoose";
 
-@modelOptions({ schemaOptions: { timestamps: true } })
-export class User {
-  public _id?: string;
+// @modelOptions({ schemaOptions: { timestamps: true } })
+// export class User {
+//   public _id?: string;
 
-  @prop({ required: true })
-  public name!: string;
+//   @prop({ required: true })
+//   public name!: string;
 
-  @prop({ required: true, unique: true })
-  public email!: string;
+//   @prop({ required: true, unique: true })
+//   public email!: string;
 
-  @prop({ required: true })
-  public password!: string;
+//   @prop({ required: true })
+//   public password!: string;
 
-  @prop({ required: true, default: false })
-  public isAdmin!: boolean;
+//   @prop({ required: true, default: false })
+//   public isAdmin!: boolean;
+// }
+
+// export const UserModel = getModelForClass(User);
+
+import { Schema, model } from "mongoose";
+
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
 }
 
-export const UserModel = getModelForClass(User);
+export const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true, default: false },
+});
+
+export const UserModel = model<IUser>("User", userSchema);
