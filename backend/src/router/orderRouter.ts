98075125c0cbe2fb01c9527983011c5056bddc48
sushtsdvg <sushtsdvg@gmail.com>
2024-02-26 17:30:03 +0530

    Admin Dashboard

--- backend/src/router/orderRouter.ts
+++ backend/src/router/orderRouter.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import { Order, OrderModel } from "../models/orderModel";
-import { Product } from "../models/productModel";
+import { Product, ProductModel } from "../models/productModel";
+import { UserModel } from "../models/userModel";
 import { isAdmin, isAuth } from "../utils";
 
 export const orderRouter = express.Router();
@@ -19,6 +20,50 @@ orderRouter.get("/", isAuth, isAdmin, async (req, res) => {
   const orders = await OrderModel.find().populate("user", "name");
   res.status(200).send(orders);
 });
+
+orderRouter.get(
+  "/summary",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req: Request, res: Response) => {
+    const orders = await OrderModel.aggregate([
+      {
+        $group: {
+          _id: null,
+          numOrders: { $sum: 1 },
+          totalSales: { $sum: "$totalPrice" },
+        },
+      },
+    ]);
+    const users = await UserModel.aggregate([
+      {
+        $group: {
+          _id: null,
+          numUsers: { $sum: 1 },
+        },
+      },
+    ]);
+    const dailyOrders = await OrderModel.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+          orders: { $sum: 1 },
+          sales: { $sum: "$totalPrice" },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+    const productCategories = await ProductModel.aggregate([
+      {
+        $group: {
+          _id: "$category",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    res.send({ users, orders, dailyOrders, productCategories });
+  })
+);
 
 orderRouter.get(
   // /api/orders/:id
